62625d0c25e26da2deb072f4aa9b54f913ddf3fc
rizqul81 <rizqul81@gmail.com>
2025-04-28 09:33:02 +0700

    Add files via upload

---
+++ TugasWebPython_MuhammadRizqul_140910240004/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route untuk halaman beranda
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route untuk halaman about me
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route untuk halaman contact
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
