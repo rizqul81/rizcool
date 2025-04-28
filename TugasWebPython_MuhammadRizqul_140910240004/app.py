64c63dabd6ebc542e85cf0f3b90407b6b7f54f36
rizqul81 <rizqul81@gmail.com>
2025-04-28 09:44:54 +0700

    Add files via upload

--- TugasWebPython_MuhammadRizqul_140910240004/app.py
+++ TugasWebPython_MuhammadRizqul_140910240004/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
+import os
+app = Flask(_name_, template_folder='templates')
 
 # Route untuk halaman beranda
 @app.route('/')
@@ -19,3 +19,4 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    import os
