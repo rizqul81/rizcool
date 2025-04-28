175d9a2e56e76d6dcbe147070862e7f99465852f
rizqul81 <rizqul81@gmail.com>
2025-04-28 09:52:32 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+app = Flask(_name_, template_folder='templates')
+
+# Route untuk halaman beranda
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route untuk halaman about me
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route untuk halaman contact
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    import os
